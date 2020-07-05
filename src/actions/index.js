9c80fff1b31f74b91b3326026499c3848dccd3ea
Norimitsu-Shimizu <sparesound31@gmail.com>
2020-07-05 15:14:15 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'ICREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: 'ICREMENT'
+})
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
