ee31d4586ce8081d51731e13c3533e8d85670271
Norimitsu-Shimizu <sparesound31@gmail.com>
2020-07-05 18:16:52 +0900

    INCREMENTS and DECREMENTS change READ_EVENTS

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -3,6 +3,11 @@ import { connect } from 'react-redux'
 
 import { increment, decrement } from '../actions'
 class EventsIndex extends Component {
+  componentDidMount() {
+    console.log("hi!")
+    this.props.readEvents()
+  }
+
   render() {
     const props = this.props
     return (
@@ -17,6 +22,6 @@ class EventsIndex extends Component {
 
 const mapStateToProps = state => ({ value: state.count.value })
 
-const mapDispatchToProps = ({ increment, decrement })
+const mapDispatchToProps = ({ readEvents })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
